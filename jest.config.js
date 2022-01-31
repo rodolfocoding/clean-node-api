8d25dcdd543b9029d5f34ad42cedc1f91ac9eeb6
Rodolfo Henrique <rodolfo.coding@gmail.com>
2022-01-31 16:42:01 -0300

    test: add rule validate param email

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,18 @@ describe('SignUp Controller', () => {
     expect(htppResponse.statusCode).toBe(400)
     expect(htppResponse.body).toEqual(new Error('Missing param: name'))
   })
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const htppRequest = {
+      body: {
+        name: 'any_name',
+        password: '123456',
+        passwordConfirmation: '123456'
+      }
+    }
+    const htppResponse = sut.handle(htppRequest)
+
+    expect(htppResponse.statusCode).toBe(400)
+    expect(htppResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
