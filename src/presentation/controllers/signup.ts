96bb5a05ba00a96e3a29e86ddab9bc402c8ac67a
Rodolfo Henrique <rodolfo.coding@gmail.com>
2022-01-31 16:41:05 -0300

    feat: add rule validate param email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 export class SignUpController {
   handle (htppRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!htppRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!htppRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
