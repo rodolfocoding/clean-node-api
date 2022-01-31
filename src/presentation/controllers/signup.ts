5e710ce3d51608b122509abf4688b2cb69fdbb5f
Rodolfo Henrique <rodolfo.coding@gmail.com>
2022-01-31 18:04:05 -0300

    refactor: add folder error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,19 @@
 import { HttpReponse, HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
 
 export class SignUpController {
   handle (htppRequest: HttpRequest): HttpReponse {
     if (!htppRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name')
+        body: new MissingParamError('name')
       }
     }
 
     if (!htppRequest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: email')
+        body: new MissingParamError('email')
       }
     }
 
