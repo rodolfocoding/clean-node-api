578a491ffbce74889a897b7b6bfbb5127d421490
Rodolfo Henrique <rodolfo.coding@gmail.com>
2022-01-31 17:33:43 -0300

    refactor: ensure SingUpController.handle implement correct infterface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { HttpReponse, HttpRequest } from '../protocols/http'
+
 export class SignUpController {
-  handle (htppRequest: any): any {
+  handle (htppRequest: HttpRequest): HttpReponse {
     if (!htppRequest.body.name) {
       return {
         statusCode: 400,
@@ -12,6 +14,11 @@ export class SignUpController {
         statusCode: 400,
         body: new Error('Missing param: email')
       }
+    }
+
+    return {
+      statusCode: 200,
+      body: 'ok'
     }
   }
 }
---
+++ src/presentation/protocols/http.ts
@@ -0,0 +1,8 @@
+export interface HttpReponse {
+  statusCode: number
+  body: any
+}
+
+export interface HttpRequest {
+  body?: any
+}
