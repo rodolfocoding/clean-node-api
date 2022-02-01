45d5f55d241bac60390bc6f4be5910d78f6514f3
Rodolfo Henrique <rodolfo.coding@gmail.com>
2022-02-01 10:57:15 -0300

    test: create test validate passwordConfirmatio require

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -39,11 +39,25 @@ describe('SignUp Controller', () => {
         name: 'any_name',
         passwordConfirmation: '123456'
       }
-
     }
     const htppResponse = sut.handle(htppRequest)
 
     expect(htppResponse.statusCode).toBe(400)
     expect(htppResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = new SignUpController()
+    const htppRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        name: 'any_name',
+        password: '123456'
+      }
+    }
+    const htppResponse = sut.handle(htppRequest)
+
+    expect(htppResponse.statusCode).toBe(400)
+    expect(htppResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 })
