3d01f840c3e2e905bd48211cc82ad6f354b03637
Rodolfo Henrique <rodolfo.coding@gmail.com>
2022-02-03 12:43:48 -0300

    test: ensure Service calls LoadFaceBookUserApi with correct params

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  testMatch: ['**/*.spec.ts']
 }
