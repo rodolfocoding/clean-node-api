d2a7316f8d8eb1d6154e564d7e276652716334cd
Rodolfo Henrique <rodolfo.coding@gmail.com>
2022-02-03 13:15:26 -0300

    refactor: move FacebookAuthenticationService

---
+++ src/data/services/facebook-authentication.ts
@@ -0,0 +1,11 @@
+import { FacebookAuthentication } from '@/domain/features'
+import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { AuthenticationError } from '@/domain/errors'
+
+export class FacebookAuthenticationService {
+  constructor (private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
+  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
+    await this.loadFacebookUserApi.loadUser(params)
+    return new AuthenticationError()
+  }
+}
